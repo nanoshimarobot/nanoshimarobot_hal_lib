01e17ba594747e96bf08129cd19fa760cc14f770
nanoshimarobot <toyozoo1225@gmail.com>
2022-05-14 03:31:12 +0900

    vector

--- motordriver_base.hpp
+++ motordriver_base.hpp
@@ -13,28 +13,31 @@ namespace nanoshimarobot_hal_lib{
     class motordriver_base{
         public:
             motordriver_base(TIM_HandleTypeDef *handle, uint32_t ch[]){
+                motor_output_ch_.push_back({ pwmOut(handle, ch[0]), pwmOut(handle, ch[1]) });
+                motor_output_ch_.push_back({ pwmOut(handle, ch[2]), pwmOut(handle, ch[3]) });
                 // motor_output_ch_.at(0) = { pwmOut(handle, ch[0]), pwmOut(handle, ch[1]) };
                 // motor_output_ch_.at(1) = { pwmOut(handle, ch[2]), pwmOut(handle, ch[3]) };
                 // std::array<std::tuple<pwmOut, pwmOut>, 2> motor_output_ch = {
                 //     {pwmOut(handle, ch[0]), pwmOut(handle, ch[1])},
                 //     {pwmOut(handle, ch[2]), pwmOut(handle, ch[3])}
                 // };
-                motor_output_ch_.at(0) = std::make_pair(pwmOut(handle, ch[0]), pwmOut(handle, ch[1]));
-                motor_output_ch_.at(1) = std::make_pair(pwmOut(handle, ch[2]), pwmOut(handle, ch[3]));
+                // motor_output_ch_.at(0) = std::make_pair(pwmOut(handle, ch[0]), pwmOut(handle, ch[1]));
+                // motor_output_ch_.at(1) = std::make_pair(pwmOut(handle, ch[2]), pwmOut(handle, ch[3]));
                 // motor_output_ch_ = std::move(motor_output_ch);
             }
 
             void write(float duty, uint8_t motor_output_n){
-                if(duty >= 0){
+                /*if(duty >= 0){
                     std::get<0>(motor_output_ch_[motor_output_n]).write(duty);
                     std::get<1>(motor_output_ch_[motor_output_n]).write(0);
                 }else{
                     std::get<0>(motor_output_ch_[motor_output_n]).write(0);
                     std::get<1>(motor_output_ch_[motor_output_n]).write(std::fabs(duty));
-                }
+               }*/
             }
 
         private:
-            std::array<std::pair<pwmOut, pwmOut>, 2> motor_output_ch_;
+            // std::array<std::pair<pwmOut, pwmOut>, 2> motor_output_ch_;
+            std::vector<std::tuple<pwmOut, pwmOut>> motor_output_ch_;
     };
 }
