386bedad25025097533c7a7112ae815cd8b7c0c8
nanoshimarobot <toyozoo1225@gmail.com>
2022-05-14 02:21:09 +0900

    bug

--- motordriver_base.hpp
+++ motordriver_base.hpp
@@ -9,19 +9,20 @@
 #define pwm_ch_num 4
 
 namespace nanoshimarobot_hal_lib{
+    #define motor_output_0_ 0
+    #define motor_output_1_ 1
     class motordriver_base{
-        public: 
-            const uint8_t motor_output_0_ = 0;
-            const uint8_t motor_output_1_ = 1;
-
+        public:
             motordriver_base(TIM_HandleTypeDef *handle, std::array<uint32_t, pwm_ch_num> ch):
             {
-                for(uint8_t i = 0; i < pwm_ch_num; ++i){
-                    pwm_handler_[i] = pwmOut(handle, ch[i]);
-                    pwm_handler_[i].write(0.0);
-                }
-                motor_output_ch_[0] = std::make_tuple(pwm_handler_[0], pwm_handler_[1]);
-                motor_output_ch_[1] = std::make_tuple(pwm_handler_[2], pwm_handler_[3]);
+                motor_output_ch_[0] = std::make_tuple(pwmOut(handle, ch[0]), pwmOut(handle, ch[1]));
+                motor_output_ch_[1] = std::make_tuple(pwmOut(handle, ch[2]), pwmOut(handle, ch[3]));
+                // for(uint8_t i = 0; i < pwm_ch_num; ++i){
+                //     pwm_handler_[i] = pwmOut(handle, ch[i]);
+                //     pwm_handler_[i].write(0.0);
+                // }
+                // motor_output_ch_[0] = std::make_tuple(pwm_handler_[0], pwm_handler_[1]);
+                // motor_output_ch_[1] = std::make_tuple(pwm_handler_[2], pwm_handler_[3]);
             }
 
             void write(float duty, uint8_t motor_output_n){
@@ -35,7 +36,6 @@ namespace nanoshimarobot_hal_lib{
             }
 
         private:
-            std::array<pwmOut, pwm_ch_num> pwm_handler_;
-            const std::array<std::tuple<pwmOut, pwmOut>, 2> motor_output_ch_;
+            std::array<std::tuple<pwmOut, pwmOut>, 2> motor_output_ch_;
     };
 }
