96c7c2987e10d8ef605f007f77ae14261a37516c
nanoshimarobot <toyozoo1225@gmail.com>
2022-05-14 02:42:26 +0900

    ch

--- motordriver_base.hpp
+++ motordriver_base.hpp
@@ -13,14 +13,13 @@ namespace nanoshimarobot_hal_lib{
     #define motor_output_1_ 1
     class motordriver_base{
         public:
-            motordriver_base(TIM_HandleTypeDef *handle, std::array<uint32_t, 4> ch):
-            handle_(handle),
-            ch_(ch)
+            template <typename T>
+            motordriver_base(TIM_HandleTypeDef *handle, uint32_t ch[])
             {
                 // motor_output_ch_[0] = std::make_tuple(pwmOut(handle_, ch_[0]), pwmOut(handle_, ch_[1]));
                 // motor_output_ch_[1] = std::make_tuple(pwmOut(handle_, ch_[2]), pwmOut(handle_, ch_[3]));
-                motor_output_ch_[0] = { pwmOut(handle_, ch_[0]), pwmOut(handle_, ch_[1]) };
-                motor_output_ch_[1] = { pwmOut(handle_, ch_[2]), pwmOut(handle_, ch_[3]) };
+                motor_output_ch_[0] = { pwmOut(handle_, ch[0]), pwmOut(handle_, ch[1]) };
+                motor_output_ch_[1] = { pwmOut(handle_, ch[2]), pwmOut(handle_, ch[3]) };
                 // for(uint8_t i = 0; i < pwm_ch_num; ++i){
                 //     pwm_handler_[i] = pwmOut(handle, ch[i]);
                 //     pwm_handler_[i].write(0.0);
@@ -41,7 +40,6 @@ namespace nanoshimarobot_hal_lib{
 
         private:
             TIM_HandleTypeDef *handle_;
-            std::array<uint32_t, 4> ch_;
             std::array<std::tuple<pwmOut, pwmOut>, 2> motor_output_ch_;
     };
 }
