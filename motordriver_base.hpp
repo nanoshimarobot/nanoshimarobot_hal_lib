053555552b2b66e906a6ff5f632b7d536e343c02
nanoshimarobot <toyozoo1225@gmail.com>
2022-05-14 11:01:28 +0900

    monya

--- motordriver_base.hpp
+++ motordriver_base.hpp
@@ -12,9 +12,11 @@
 namespace nanoshimarobot_hal_lib{
     class motordriver_base{
         public:
-            motordriver_base(TIM_HandleTypeDef *handle, uint32_t ch[]){
-                motor_output_ch_.push_back({ pwmOut(handle, ch[0]), pwmOut(handle, ch[1]) });
-                motor_output_ch_.push_back({ pwmOut(handle, ch[2]), pwmOut(handle, ch[3]) });
+            motordriver_base(TIM_HandleTypeDef *handle, uint32_t ch[]):
+            motor_output_ch_({{ pwmOut(handle, ch[0]), pwmOut(handle, ch[1]) },{ pwmOut(handle, ch[2]), pwmOut(handle, ch[3]) }} )
+            {
+                // motor_output_ch_.push_back({ pwmOut(handle, ch[0]), pwmOut(handle, ch[1]) });
+                // motor_output_ch_.push_back({ pwmOut(handle, ch[2]), pwmOut(handle, ch[3]) });
                 // motor_output_ch_.at(0) = { pwmOut(handle, ch[0]), pwmOut(handle, ch[1]) };
                 // motor_output_ch_.at(1) = { pwmOut(handle, ch[2]), pwmOut(handle, ch[3]) };
                 // std::array<std::tuple<pwmOut, pwmOut>, 2> motor_output_ch = {
@@ -23,10 +25,16 @@ namespace nanoshimarobot_hal_lib{
                 // };
                 // motor_output_ch_.at(0) = std::make_pair(pwmOut(handle, ch[0]), pwmOut(handle, ch[1]));
                 // motor_output_ch_.at(1) = std::make_pair(pwmOut(handle, ch[2]), pwmOut(handle, ch[3]));
-                // motor_output_ch_ = std::move(motor_output_ch);
             }
 
             void write(float duty, uint8_t motor_output_n){
+                // if(duty >= 0){
+                //     std::get<0>(motor_output_ch_[motor_output_n]).write(duty);
+                //     std::get<1>(motor_output_ch_[motor_output_n]).write(0);
+                // }else{
+                //     std::get<0>(motor_output_ch_[motor_output_n]).write(0);
+                //     std::get<1>(motor_output_ch_[motor_output_n]).write(std::fabs(duty));
+                // }
                 /*if(duty >= 0){
                     std::get<0>(motor_output_ch_[motor_output_n]).write(duty);
                     std::get<1>(motor_output_ch_[motor_output_n]).write(0);
@@ -37,7 +45,7 @@ namespace nanoshimarobot_hal_lib{
             }
 
         private:
-            // std::array<std::pair<pwmOut, pwmOut>, 2> motor_output_ch_;
-            std::vector<std::tuple<pwmOut, pwmOut>> motor_output_ch_;
+            std::array<std::tuple<pwmOut, pwmOut>, 2> motor_output_ch_;
+            // std::vector<std::tuple<pwmOut, pwmOut>> motor_output_ch_;
     };
 }
